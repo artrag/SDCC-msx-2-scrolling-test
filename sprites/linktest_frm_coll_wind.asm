330095a26066d60b0cd160b1f2f90ac6300c908d
artrag <artrag@users.noreply.github.com>
2022-02-14 23:43:19 +0100

    sprite support

---
+++ sprites/linktest_frm_coll_wind.asm
@@ -0,0 +1,14 @@
+linktest_coll_wind:
+    defb 1,15,10,31 
+    defb 1,15,10,31 
+    defb 1,15,10,31 
+    defb 1,15,10,31 
+    defb 2,14,9,31 
+    defb 2,14,9,31 
+    defb 2,14,8,31 
+    defb 2,14,9,31 
+    defb 3,13,8,31 
+    defb 3,13,8,31 
+    defb 3,13,8,31 
+    defb 3,13,8,31 
+
